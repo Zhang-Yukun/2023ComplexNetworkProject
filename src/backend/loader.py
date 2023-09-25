b244e7399e19bb4bc8ae1d97f9efc1753732beb5
wb <2637609512@qq.com>
2023-09-25 20:35:20 +0800

    缺失模块上传1

---
+++ src/backend/loader.py
@@ -0,0 +1,134 @@
+import os.path
+import pickle
+import random
+import sys
+from os import path
+
+from src.backend.model.graph import Graph
+from src.backend.process import generate_novel_graph
+
+current_path = os.path.dirname(__file__)
+print(current_path)
+def generate_random_graph(size):
+    g = Graph()
+    for i in range(size):
+        g.add_node("node{}".format(i), "node{}".format(i))
+    rdm = random.Random()
+    for i in range(size * (size - 1) // 2):
+        if rdm.random() > 0.8:
+            rf = [i for i in g.id2nodes.keys()][rdm.randint(0, g.size - 1)]
+            rt = [i for i in g.id2nodes.keys()][rdm.randint(0, g.size - 1)]
+            while rf == rt:
+                rf = [i for i in g.id2nodes.keys()][rdm.randint(0, g.size - 1)]
+                rt = [i for i in g.id2nodes.keys()][rdm.randint(0, g.size - 1)]
+            g.add_edge(rf, rt, rdm.randint(1, 3))
+    return g
+
+
+def load_graph(clz, label):
+    sys.setrecursionlimit(3000)
+    g = clz()
+    if path.exists("backend/model/cache/{}.pkl".format(label)):
+        print('load graph {} from disk...'.format(label))
+        return pickle.load(open("backend/model/cache/{}.pkl".format(label), 'rb'))
+    # if not path.exists('backend/data/parsed/{}/names.txt'.format(label)):
+    #     generate_novel_graph(label)
+    with open(current_path+ '/data/parsed/{}/names.txt'.format(label), 'r',encoding='utf8') as f:
+        for x in f.readlines():
+            g.add_node(x.strip(), x.strip())
+    with open(current_path+'/data/parsed/{}/edges.txt'.format(label), 'r',encoding='utf8') as f:
+        for ed in f.readlines():
+            ed = ed.split(";")
+            n1 = ed[0].strip()
+            n2 = ed[1].strip()
+            g.add_edge(n1, n2, float(ed[2].strip()))
+    print('calculating properties for graph {}'.format(label))
+    g.calc_dists()
+    g.calc_coreness()
+    g.calc_cluster_coefficient()
+    print('calculation properties for graph {} done'.format(label))
+    if not os.path.exists("backend/model/cache/"):
+        os.makedirs("backend/model/cache/")
+    pickle.dump(g, open("backend/model/cache/{}.pkl".format(label), "wb"))
+    return g
+
+
+class RedGraph(Graph):
+    # def get_node_color_map(self):
+    #     map = super().get_node_color_map()
+    #     map['贾'] = "#26A7FF"
+    #     map['史'] = "#3abc98"
+    #     map['林'] = "#EC4A94"
+    #     map['王'] = "#ECAD00"
+    #     map['靴'] = "#F0AA34"
+    #     return map
+
+    def get_node_class(self, node):
+        return node.label[0]
+
+
+class KingdomGraph(Graph):
+    def __init__(self):
+        super().__init__()
+        self.country_map = {}
+        with open('backend/data/parsed/kingdom/countries/qun.txt', 'r') as f:
+            for name in f.readlines():
+                self.country_map[name.strip()] = 'qun'
+        with open('backend/data/parsed/kingdom/countries/wei.txt', 'r') as f:
+            for name in f.readlines():
+                self.country_map[name.strip()] = 'wei'
+        with open('backend/data/parsed/kingdom/countries/shu.txt', 'r') as f:
+            for name in f.readlines():
+                self.country_map[name.strip()] = 'shu'
+        with open('backend/data/parsed/kingdom/countries/wu.txt', 'r') as f:
+            for name in f.readlines():
+                self.country_map[name.strip()] = 'wu'
+
+    # def get_node_color_map(self):
+    #     return {
+    #         'wei': "#097AFF",
+    #         "shu": "#FD9A28",
+    #         "wu": "#3abc98",
+    #         "qun": "#C1CBCA"
+    #     }
+
+    def get_node_class(self, node):
+        if node.label in self.country_map.keys():
+            return self.country_map[node.label]
+        else:
+            return 'qun'
+
+
+class WestGraph(Graph):
+    def get_node_class(self, node):
+        return node.label[0]
+
+
+class WhoGraph(Graph):
+    def get_node_class(self, node):
+        return node.label[0]
+
+class RailGraph(Graph):
+    def get_node_class(self, node):
+        return node.label[0]
+
+def load_kingdom_graph():
+    return load_graph(KingdomGraph, "kingdom")
+
+
+def load_red_graph():
+    return load_graph(RedGraph, "red")
+
+
+def load_west_graph():
+    return load_graph(WestGraph, "west")
+
+
+def load_who_graph() -> object:
+    return load_graph(WhoGraph, "who")
+
+def load_rail_graph() -> object:
+    return load_graph(RailGraph, "rail")
+
+def load_any_graph(key) -> object:
+    return load_graph(Graph, key)
