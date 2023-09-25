cb02cebec7244dadca53a876728ad9d4ade5d5ba
Zhang-Yukun <2936501245@qq.com>
2023-09-25 23:25:52 +0800

    Modify: Delete some files.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from src.backend.process import process_csv, generate_novel_graph
 from src.front.app import app
 
 if __name__ == "__main__":
     app.run_server(host='127.0.0.1', port='8082', debug=True, dev_tools_ui=False)
-    #  generate_novel_graph('who')
--- src/backend/loader.py
+++ src/backend/loader.py
@@ -6,10 +6,8 @@ from os import path
 import pandas as pd
 
 from src.backend.model.graph import Graph
-from src.backend.process import generate_novel_graph
 
-current_path = os.path.dirname(__file__)
-print(current_path)
+
 def generate_random_graph(size):
     g = Graph()
     for i in range(size):
@@ -27,36 +25,28 @@ def generate_random_graph(size):
 
 
 def load_graph(clz, label):
+    base_path = "/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/"
     sys.setrecursionlimit(3000)
     g = clz()
-    if path.exists("/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/cache/{}.pkl".format(label)):
+    if path.exists(base_path + "cache/{}.pkl".format(label)):
         print('load graph {} from disk...'.format(label))
-        return pickle.load(open("/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/cache/{}.pkl".format(label), 'rb'))
+        return pickle.load(open(base_path + "/cache/{}.pkl".format(label), 'rb'))
     # if not path.exists('backend/data/parsed/{}/names.txt'.format(label)):
     #     generate_novel_graph(label)
-    df = pd.read_csv("/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/node.csv")
+    df = pd.read_csv(base_path + "node.csv")
     for index, row in df.iterrows():
         g.add_node(row["node"], row["node"])
-    df = pd.read_csv("/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/edge_simple.csv")
+    df = pd.read_csv(base_path + "edge_simple.csv")
     for index, row in df.iterrows():
         g.add_edge(row["start_station_name"], row["end_station_name"], float(row["running_time"]))
-    # with open(current_path+ '/data/parsed/{}/names.txt'.format(label), 'r',encoding='utf8') as f:
-    #     for x in f.readlines():
-    #         g.add_node(x.strip(), x.strip())
-    # with open(current_path+'/data/parsed/{}/edges.txt'.format(label), 'r',encoding='utf8') as f:
-    #     for ed in f.readlines():
-    #         ed = ed.split(";")
-    #         n1 = ed[0].strip()
-    #         n2 = ed[1].strip()
-    #         g.add_edge(n1, n2, float(ed[2].strip()))
     print('calculating properties for {}'.format(label))
     g.calc_dists()
     g.calc_coreness()
     g.calc_cluster_coefficient()
     print('calculation properties for {} done'.format(label))
-    if not os.path.exists("/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/cache/"):
-        os.makedirs("/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/cache/")
-    pickle.dump(g, open("/Applications/LANGUAGE/PYTHON/2023ComplexNetworkProject/data/cache/{}.pkl".format(label), "wb"))
+    if not os.path.exists(base_path + "cache/"):
+        os.makedirs(base_path + "cache/")
+    pickle.dump(g, open(base_path + "cache/{}.pkl".format(label), "wb"))
     return g
 
 class RailGraph(Graph):
